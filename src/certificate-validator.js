95e0e12120243f67d7a24ebc5cc14a5fcdd6588d
KikoCis <kikocisneros@gmail.com>
2024-12-03 09:36:14 +0100

    FIX

--- src/certificate-validator.js
+++ src/certificate-validator.js
@@ -1,16 +1,14 @@
 import { readFileSync } from 'fs';
 import forge from 'node-forge';
 
-const { x509 } = forge;
-
 export async function validateCertificate(certPath) {
   try {
     const certPEM = readFileSync(certPath, 'utf8');
-    const cert = x509.certificateFromPem(certPEM);
+    const cert = forge.pki.certificateFromPem(certPEM);
     
     return {
-      subject: cert.subject,
-      issuer: cert.issuer,
+      subject: formatDN(cert.subject),
+      issuer: formatDN(cert.issuer),
       validFrom: cert.validity.notBefore,
       validTo: cert.validity.notAfter,
       serialNumber: cert.serialNumber
@@ -18,6 +16,14 @@ export async function validateCertificate(certPath) {
   } catch (error) {
     throw new Error(`Error validating certificate: ${error.message}`);
   }
+}
+
+// Función auxiliar para formatear el DN (Distinguished Name)
+function formatDN(dn) {
+  return Object.entries(dn.attributes).reduce((acc, [_, attr]) => {
+    acc[attr.shortName || attr.name] = attr.value;
+    return acc;
+  }, {});
 }
 
 export function isEVSSL(certInfo) {
